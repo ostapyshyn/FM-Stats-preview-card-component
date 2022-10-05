869464fd1cbc29af2e632ee7c20b035092302b13
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-05 23:53:42 +0300

    Fix margins

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -7,27 +7,30 @@ function StatsCard() {
   return (
     <main>
       <article>
-        <div className={styles.info}>
-          <h1>
-            Get <span>insights</span> that help your business grow.
-          </h1>
-          <p>
-            Discover the benefits of data analytics and make better decisions regarding revenue,
-            customer experience, and overall efficiency.
-          </p>
+        <div className={styles.left}>
+          <div className={styles.info}>
+            <h1>
+              Get <span>insights</span> that help your business grow.
+            </h1>
+            <p>
+              Discover the benefits of data analytics and make better decisions regarding revenue,
+              customer experience, and overall efficiency.
+            </p>
 
-          <ul>
-            <li>
-              10k+ <span>Companies</span>
-            </li>
-            <li>
-              314 <span>Templates</span>
-            </li>
-            <li>
-              12M+ <span>Queries</span>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                10k+ <span>Companies</span>
+              </li>
+              <li>
+                314 <span>Templates</span>
+              </li>
+              <li>
+                12M+ <span>Queries</span>
+              </li>
+            </ul>
+          </div>
         </div>
+
         <div className={styles.image}>
           <picture>
             <source media="(min-width: 899px)" srcSet={employee} />
